93e86b1ae587459c5ab9e5f55089343f6dcc74a0
Aleksa2024 <solevic.aleksa@yahoo.com>
2024-11-06 15:20:01 +0100

    created started game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  const startWindow = document.getElementById("start-window");
+  const gameWindow = document.getElementById("game-window");
+  const startBtn = document.getElementById("start-btn");
+  let gameActive = false;
 
-    console.log("Hello World!");
-    
+  function startGame() {
+    gameActive = true;
+    if (gameActive === true) {
+        startWindow.style.display = 'none';
+        gameWindow.style.display = 'flex';
+    }
+  }
+    startBtn.addEventListener('click', function () {
+        // startGame();
+
+        console.log('click!');
+    });
 });
