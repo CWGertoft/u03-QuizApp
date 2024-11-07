608539fa80e34adf48ba47c140a86ee2e05d6800
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2024-11-07 13:04:45 +0100

    Add js changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
   const startWindow = document.getElementById("start-window");
   const gameWindow = document.getElementById("game-window");
+  const question = document.getElementById("question")
   const startBtn = document.getElementById("start-btn");
+  const input = document.getElementById("name-input");
+  // const url = "https://opentdb.com/api.php?amount=50&category=9";
+  const url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple";
+
+  let userName = "";
   let gameActive = false;
 
+
+  async function getQuizData() {
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      const data = await response.json();
+      console.log(data);
+      // return data.results;
+      showQuestions(data.results)
+
+    } catch(error) {
+      console.log(error);
+    };
+
+  }
+
+  function showQuestions(data) {
+    for (let i=0; i < data.length; i++){
+      console.log(data[i].question);
+    }
+    
+  }
+
+  
+
+
+  getQuizData();
+
   function startGame() {
+    userName = input.value;
+    console.log(userName);
     gameActive = true;
     if (gameActive === true) {
        // startWindow.style.display = 'none';
@@ -15,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
     startBtn.addEventListener('click', function () {
-        // startGame();
+        startGame();
 
         console.log('click!');
     });
