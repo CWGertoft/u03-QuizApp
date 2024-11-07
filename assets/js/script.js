98ef481a6657190ef75e86095038ce312876f066
sara-jpallander <sara.j.pallander@gmail.com>
2024-11-07 15:30:29 +0100

    finish function that picks a random question and answer and displays them

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,120 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const startWindow = document.getElementById("start-window");
-  const gameWindow = document.getElementById("game-window");
-  const question = document.getElementById("question");
-  const startBtn = document.getElementById("start-btn");
-  const input = document.getElementById("name-input");
 
-  
-  // const url = "https://opentdb.com/api.php?amount=50&category=9";
-  const url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple";
+    const startWindow = document.getElementById("start-window");
+    const gameWindow = document.getElementById("game-window");
+    const startBtn = document.getElementById("start-btn");
 
-  let userName = "";
-  let gameActive = false;
+    const input = document.getElementById("name-input");
+    let userName = "";
+    let gameActive = false;
 
 
-  async function getQuizData() {
+    const question = document.getElementById("question");
+    const option1 = document.getElementById("option1");
+    const option2 = document.getElementById("option2");
 
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      const data = await response.json();
-      console.log(data);
-      // return data.results;
-      showQuestion(data.results);
+    
+    // const url = "https://opentdb.com/api.php?amount=50&category=9";
+    // const url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple";
+    const url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=boolean";
 
-    } catch(error) {
-      console.log(error);
+
+
+    async function getQuizData() {
+
+        try {
+
+            const response = await fetch(url);
+
+            if (!response.ok) {
+
+                throw new Error(response.status);
+            };
+
+            const data = await response.json();
+            console.log(data);
+
+            showQuestion(data.results);
+
+        } catch(error) {
+
+            console.log(error);
+        };
+
     };
 
-  }
+    function showQuestion(data) {
+    
+        let random = Math.floor(Math.random() * data.length);
+        let randomQuestion = "";
+        let wrongAnswer = "";
+        let correctAnswer = "";
 
-  function showQuestion(data) {
-    
-    let random = Math.floor(Math.random() * data.length);
-    let randomQuestion = "";
-    
-    for (let i = 0; i < data.length; i++){
-      
-      if (i === random) {
-        console.log(data[i].question);
-        randomQuestion = data[i].question;
-      };
+        let optionTrue = "false";
+        let optionFalse = "";
         
+        for (let i = 0; i < data.length; i++){
+        
+            if (i === random) {
+            
+                randomQuestion = data[i].question;
+                console.log(data[i].question);
+
+                wrongAnswer = data[i].incorrect_answers.pop();
+
+                correctAnswer = data[i].correct_answer;
+
+                /* För att rätt och fel svar inte ska komma i en bestämd ordning
+                   så läggs dom i en variabel som är för alternativet true och
+                   en variabel för alternativet false. /sjp */
+
+                if (wrongAnswer === "True") {
+
+                    optionTrue = wrongAnswer;
+
+                } else if (correctAnswer === "True") {
+
+                    optionTrue = correctAnswer;
+
+                };
+                
+                if (wrongAnswer === "False") {
+
+                    optionFalse = wrongAnswer;
+
+                } else if (correctAnswer === "False") {
+
+                    optionFalse = correctAnswer;
+                };
+
+            };
+        };
+        
+        question.innerHTML = randomQuestion;
+
+        option1.innerText = optionTrue;
+        option2.innerText = optionFalse;
+
     };
 
-    question.innerHTML = randomQuestion;
 
-  };
-
-    /* function displayQuestion(question)  */
-
-  
+    getQuizData();
 
 
-  getQuizData();
+    function startGame() {
+        userName = input.value;
+        console.log(userName);
+        gameActive = true;
+        if (gameActive === true) {
+        // startWindow.style.display = 'none';
+        // gameWindow.style.display = 'flex';
 
-  function startGame() {
-    userName = input.value;
-    console.log(userName);
-    gameActive = true;
-    if (gameActive === true) {
-       // startWindow.style.display = 'none';
-       // gameWindow.style.display = 'flex';
+        /* Kommenterat ut koden eftersom den inte är funktionell just nu då vi inte lagt
+            till någon styling än. /sjp */
+        }
+    };
 
-       /* Kommenterat ut koden eftersom den inte är funktionell just nu då vi inte lagt
-          till någon styling än. /sjp */
-    }
-  }
-    startBtn.addEventListener('click', function () {
+    startBtn.addEventListener('click', function() {
         startGame();
 
         console.log('click!');
