45e8dee531b5e7f4ba4f1a3b937d3f6a08e6bea3
Aleksa2024 <solevic.aleksa@yahoo.com>
2024-11-14 13:26:17 +0100

    new js-file with improved code

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -10,53 +10,41 @@ document.addEventListener("DOMContentLoaded", function () {
   let correctAnswer = "";
   let optionTrue = "";
   let optionFalse = "";
+  let currentQuestion = 0;
 
   let score = 0;
 
-  const question = document.getElementById("question");
+  let question = document.getElementById("question");
   const option1 = document.getElementById("option1");
   const option2 = document.getElementById("option2");
 
   const url = "./assets/quizQuestions.json";
 
-  async function getQuizData() {
+  async function getQuestions() {
     try {
       const response = await fetch(url);
-
       if (!response.ok) {
-        throw new Error(response.status);
+        throw new Error(`HTTP ERROR: ${response.status}`);
       }
-
       const data = await response.json();
-      //console.log(data);
-
       showQuestion(data.results);
+      // console.log(data);
     } catch (error) {
-      console.log(error);
+      console.log(error, "error");
     }
   }
 
+  getQuestions();
 
-
-
-  
-  /*---Change the code----*/
   function showQuestion(data) {
-    let random = Math.floor(Math.random() * data.length);
-    let randomQuestion = "";
-
     for (let i = 0; i < data.length; i++) {
-      if (i === random) {
-        randomQuestion = data[i].question;
-        //console.log(data[i].question);
+      // console.log(data[i].question);
+      if (i === currentQuestion) {
+        question.innerHTML = data[i].question;
 
         wrongAnswer = data[i].incorrect_answer;
 
         correctAnswer = data[i].correct_answer;
-
-        /* För att rätt och fel svar inte ska komma i en bestämd ordning
-                   så läggs dom i en variabel som är för alternativet true och
-                   en variabel för alternativet false. /sjp */
 
         if (wrongAnswer === "True") {
           optionTrue = wrongAnswer;
@@ -69,60 +57,34 @@ document.addEventListener("DOMContentLoaded", function () {
         } else if (correctAnswer === "False") {
           optionFalse = correctAnswer;
         }
+
+        option1.innerText = optionTrue;
+        option2.innerText = optionFalse;
       }
     }
-
-    question.innerHTML = randomQuestion;
-
-    option1.innerText = optionTrue;
-    option2.innerText = optionFalse;
   }
 
-  // Gör alternativen klickbara samt jämför valet med rätt eller fel svar
-  // Ger poäng för rätt svar och laddar in en ny fråga
-  // För fel svar händer inget med poängen, laddar in ny fråga.
   option1.addEventListener("click", function () {
     //console.log("click");
 
     if (optionTrue === correctAnswer) {
       score++;
       console.log("Correct");
-      showQuestion(getQuizData());
     } else if (optionTrue === wrongAnswer) {
       console.log("Wrong");
-      showQuestion(getQuizData());
     }
+    currentQuestion++;
+    getQuestions();
   });
 
   option2.addEventListener("click", function () {
     if (optionFalse === correctAnswer) {
       score++;
       console.log("Correct");
-      showQuestion(getQuizData());
     } else if (optionFalse === wrongAnswer) {
       console.log("Wrong");
-      showQuestion(getQuizData());
     }
-  });
-
-  getQuizData();
-
-  function startGame() {
-    userName = input.value;
-    console.log(userName);
-
-    gameActive = true;
-
-    if (gameActive === true) {
-      // startWindow.style.display = "none";
-      // gameWindow.style.display = 'flex';
-      /* Kommenterat ut koden eftersom den inte är funktionell just nu då vi inte lagt
-            till någon styling än. /sjp */
-    }
-  }
-
-  startBtn.addEventListener("click", function () {
-    startGame();
-    console.log("click!");
+    currentQuestion++;
+    getQuestions();
   });
 });
