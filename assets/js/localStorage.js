c8de83f9f2c9e0269c9c0df699352d2fab22ad42
sara-jpallander <sara.j.pallander@gmail.com>
2024-11-21 16:48:55 +0100

    minor adjustments

--- assets/js/localStorage.js
+++ assets/js/localStorage.js
@@ -9,11 +9,12 @@ const clearBtn = document.getElementById("clean_leaderboard");
 const highestResultsList = document.getElementById("highestResultsList");
 
 const highestScores =
-  JSON.parse(localStorage.getItem("highestScores")) || [].filter(
-    (score) =>
-      score &&
-      typeof score.name === "string" &&
-      typeof score.result === "number");
+    JSON.parse(localStorage.getItem("highestScores")) || [].filter(
+        (score) =>
+            score &&
+            typeof score.name === "string" &&
+            typeof score.result === "number");
+
 const MAX_HIGH_SCORES = 5;
 let score;
 
@@ -24,26 +25,28 @@ export function saveToLocalStorage() {
           (score) => score && score.name === storedUserName
         );
         if (existingUserIndex >= 0) {
-          if (mostRecentScore > highestScores[existingUserIndex].result) {
-            highestScores[existingUserIndex].result = mostRecentScore;
-          }
+            if (mostRecentScore > highestScores[existingUserIndex].result)
+            {
+                highestScores[existingUserIndex].result = mostRecentScore;
+            };
         } else {
-          score = {
-            name: storedUserName,
-            result: mostRecentScore,
-          };
+            score = {
+                name: storedUserName,
+                result: mostRecentScore,
+            };
     
-          highestScores.push(score);
+            highestScores.push(score);
     
-          clearBtn.style.display = "flex";
-          highestResultsList.style.display = "flex";
-          highestResultsList.style.flexDirection = "column";
+            clearBtn.style.display = "flex";
+            highestResultsList.style.display = "flex";
+            highestResultsList.style.flexDirection = "column";
         };
       
         highestScores.sort((a, b) => {
-          if (!a || !b) return 0; // Skip null or undefined values
-          return b.result - a.result;
+            if (!a || !b) return 0; // Skip null or undefined values
+            return b.result - a.result;
         });
+
         highestScores.splice(MAX_HIGH_SCORES);
         localStorage.setItem("highestScores", JSON.stringify(highestScores));
     };
@@ -57,6 +60,7 @@ export function showLeaderBoard() {
         score &&
         typeof score.name === "string" &&
         typeof score.result === "number"
+
     )
     .map((score) => `<li> ${score.name} - ${score.result} </li>`)
     .join("");
